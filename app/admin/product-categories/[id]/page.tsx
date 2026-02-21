777153d9bc66e8c5abaa10dac9f061abd2ad565c
Serista3 <stacla5282@gmail.com>
2026-02-22 01:59:31 +0700

    setup project #1

---
+++ app/admin/product-categories/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function EditProductCategory() {
+  return <div>Edit Product Category</div>;
+}
---
+++ app/admin/product-categories/new/page.tsx
@@ -0,0 +1,3 @@
+export default function newProductCategory() {
+  return <div>New Product Category</div>;
+}
